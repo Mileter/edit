b61816eb2913c3f7ab2545ed97a059fc84f8e550
Mileter <90982151+Mileter@users.noreply.github.com>
2025-01-08 22:03:23 -0800

    Fixed to use 120 character limit, instead of 79
    Signed-off-by: Mileter <90982151+Mileter@users.noreply.github.com>

--- tools/indent_all.sh
+++ tools/indent_all.sh
@@ -1,5 +1,5 @@
 # Find all source/header files
 
 find ../source -type f \( -name "*.cpp" -o -name "*.c" -o -name "*.h" -o -name "*.hpp" \) \
-  -exec printf "indent -bap -bli0 -i8 -l79 -ncs -npcs -npsl -fca -lc79 -fc1 -ts8 \"%s\"\n" {} \; \
-  -exec indent -bap -bli0 -i8 -l79 -ncs -npcs -npsl -fca -lc79 -fc1 -ts8 {} \;
+  -exec printf "indent -bap -bli0 -i8 -l120 -ncs -npcs -npsl -fca -lc79 -fc1 -ts8 \"%s\"\n" {} \; \
+  -exec indent -bap -bli0 -i8 -l120 -ncs -npcs -npsl -fca -lc79 -fc1 -ts8 {} \;
